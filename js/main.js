1a41babf68b0ea39cf9f1e4bdb0513504952b691
AceWinspire <acevojinovic@gmail.com>
2019-06-10 00:55:42 +0200

    Switched nav logic

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 $(function (side_Nav_Animation) {
 
     var navAnim = new TimelineMax({ paused: true })
-    navAnim.to("nav", 0.6, { x: 460, backgroundColor: "rgba(255, 255, 255, 1)", ease: SteppedEase.config(6) });
+    navAnim.to("#navigation", 0.6, { x: 460, backgroundColor: "rgba(255, 255, 255, 1)", ease: SteppedEase.config(6) });
     
    
     var topAnim =  TweenMax.to("#top", 1.2, {paused:true, rotation:45, ease: Elastic.easeInOut.config(1.2, 2)});
@@ -13,7 +13,7 @@ $(function (side_Nav_Animation) {
     
     
     var sidebarBox = document.querySelector('.menuicon');
-    var sidebarBtn = document.querySelector('nav');
+    var sidebarBtn = document.querySelector('#navigation');
     var pageWrapper = document.querySelector('#page-content');
     
     sidebarBtn.addEventListener('mouseenter', function (event) {
